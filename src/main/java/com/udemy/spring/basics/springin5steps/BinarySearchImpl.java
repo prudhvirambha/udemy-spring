9c92f93e62b09363d0eaa059a19d6cb2e878926c
prudhvirambha <prudhvirambha@gmail.com>
2023-08-15 13:01:08 +0530

    Using Qualifier

--- src/main/java/com/udemy/spring/basics/springin5steps/BinarySearchImpl.java
+++ src/main/java/com/udemy/spring/basics/springin5steps/BinarySearchImpl.java
@@ -1,12 +1,14 @@
 package com.udemy.spring.basics.springin5steps;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BinarySearchImpl {
 
     @Autowired
+    @Qualifier("bubble")
     private SortAlgorithm sortAlgorithm;
 
     //below is the constructor injection
--- src/main/java/com/udemy/spring/basics/springin5steps/BubblesortAlgo.java
+++ src/main/java/com/udemy/spring/basics/springin5steps/BubblesortAlgo.java
@@ -1,10 +1,11 @@
 package com.udemy.spring.basics.springin5steps;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
+@Qualifier("bubble")
 public class BubblesortAlgo implements  SortAlgorithm{
     public int[] sort(int[] numbers){
         //Logic for bubble sort
