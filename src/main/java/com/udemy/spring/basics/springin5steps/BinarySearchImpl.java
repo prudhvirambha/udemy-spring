63e500af2d51597a155b66ab22708c32716c9acd
prudhvirambha <prudhvirambha@gmail.com>
2023-08-15 13:14:44 +0530

    prototype bean scope (good practice)

--- src/main/java/com/udemy/spring/basics/springin5steps/BinarySearchImpl.java
+++ src/main/java/com/udemy/spring/basics/springin5steps/BinarySearchImpl.java
@@ -2,11 +2,13 @@ package com.udemy.spring.basics.springin5steps;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototype") //This will create a new instance
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE) //This will create a new instance
+//The above is good practice
 public class BinarySearchImpl {
 
     @Autowired
