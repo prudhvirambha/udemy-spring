1b396c39ef88df4955c467f9de81e99633a3fa72
prudhvirambha <prudhvirambha@gmail.com>
2023-08-17 12:13:26 +0530

    Step 18 - Contexts and Dependency Injection (CDI) - @Named, @Inject

---
+++ src/main/java/com/udemy/spring/basics/springin5steps/cdi/SomeCdiBusiness.java
@@ -0,0 +1,21 @@
+package com.udemy.spring.basics.springin5steps.cdi;
+
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+
+@Named
+public class SomeCdiBusiness {
+	
+	@Inject
+	SomeCdiDao someCdiDao;
+
+	public SomeCdiDao getSomeCdiDao() {
+		return someCdiDao;
+	}
+
+	public void setSomeCdiDao(SomeCdiDao someCdiDao) {
+		this.someCdiDao = someCdiDao;
+	}
+	
+	
+}
---
+++ src/main/java/com/udemy/spring/basics/springin5steps/cdi/SomeCdiDao.java
@@ -0,0 +1,8 @@
+package com.udemy.spring.basics.springin5steps.cdi;
+
+import jakarta.inject.Named;
+
+@Named
+public class SomeCdiDao {
+	
+}
